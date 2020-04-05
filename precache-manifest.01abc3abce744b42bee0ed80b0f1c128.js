e77b6f7b65c1a5dbbc83ac36f3fc7b2820079057
Wojciech Masicz <wmasicz@gmail.com>
2020-04-05 11:29:28 +0200

    Updates

---
+++ precache-manifest.01abc3abce744b42bee0ed80b0f1c128.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3528cbea4f27ae91bb96660185a54e",
+    "url": "/live-scoreboard/index.html"
+  },
+  {
+    "revision": "913ac7ed12645858a11c",
+    "url": "/live-scoreboard/static/css/main.47c1afb0.chunk.css"
+  },
+  {
+    "revision": "03aa909435ff6f48afb7",
+    "url": "/live-scoreboard/static/js/2.794e627f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/live-scoreboard/static/js/2.794e627f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913ac7ed12645858a11c",
+    "url": "/live-scoreboard/static/js/main.26bcad0f.chunk.js"
+  },
+  {
+    "revision": "5e022c8c252fdc14db72",
+    "url": "/live-scoreboard/static/js/runtime-main.f04dabe7.js"
+  }
+]);
